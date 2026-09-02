8ef845eb8f178525fe71d834fc925dc83b9dbfac
Evelymn <evelyncastroel711360@gmail.com>
2026-09-01 19:24:07 -0600

    Crear navbar y hero de la pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+// Importamos nuestro componente Navbar
+// para poder utilizarlo dentro de app.component.html.
+import { NavbarComponent } from './components/navbar/navbar.component';
+// Importamos HeroComponent para poder mostrar
+// la sección principal de presentación.
+import { HeroComponent } from './components/hero/hero.component';
+
 
 @Component({
   selector: 'app-root',
+
+  // standalone significa que este componente
+  // funciona sin necesitar un AppModule tradicional.
   standalone: true,
-  imports: [RouterOutlet],
+
+  // Aquí registramos los componentes que queremos
+  // utilizar dentro de app.component.html.
+  //
+  // Como queremos usar:
+  // <app-navbar></app-navbar>
+  //
+  // agregamos NavbarComponent.
+  imports: [
+    NavbarComponent,
+    HeroComponent
+],
+
+
   templateUrl: './app.component.html',
+
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'kakbe-lab';
+
 }
